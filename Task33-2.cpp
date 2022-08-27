d56c3e9e79aca13150ce6bfeb00f3ad6aff11bcf
ANDREJ <and423@inbox.lv>
2022-08-27 22:29:34 +0200

    Добавьте файлы проекта.

---
+++ Task33-2.cpp
@@ -0,0 +1,29 @@
+﻿
+#include "Task33-2.h"
+
+int main()
+{
+  /*
+  std::map<std::string, nlohmann::json> films{ {"Fall", film1}, {"Batmen", film2}, 
+											  {"The Contractor", film3},{"Liked", film4}, {"Fate of a Sport", film5} };
+  nlohmann::json j_film(films);
+  std::ofstream file("record.json");
+  file << std::setw(4) << films;
+  file.close();
+  */
+  
+  
+  std::ifstream inFile("record.json");
+  nlohmann::json inFilms;
+  inFile >> inFilms;
+  inFile.close();
+  std::string actor = "Virginia Gardner";
+  std::vector<std::string> actorFilms;
+  for ( nlohmann::json::iterator it = inFilms.begin(); it != inFilms.end(); ++it) {
+	std::string t(it.value()["actors"]);
+	if (t.find(actor) != std::string::npos) actorFilms.push_back(it.key());
+  }
+  for (auto& el : actorFilms) std::cout << el << "  ";
+  std::cout << std::endl;
+  return 0;
+}
---
+++ Task33-2.h
@@ -0,0 +1,53 @@
+﻿
+#pragma once
+
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <map>
+#include <vector>
+#include "nlohmann/json.hpp"
+
+
+//======================================================================
+nlohmann::json film1{
+   {"name" , "Fall"},
+   {"country creater", "USA" ,},
+   {"create date", 2022 },
+   {"director", "Scott Mann" },
+   { "actors"," Grace Fulton, Virginia Gardner , Jeffrey Dean Morgan, Brianna Hildebrand,Mason Gooding, Jasper Cole" }
+
+};
+
+nlohmann::json film2{
+  {"name", "Batman"},
+  {"country creater", "USA"},
+  {"create date", 2022},
+  {"director", "Matt Reeves"},
+  {"actors" , "Robert Pattinson, Jeffrey Wright , Zoe Kravitz, Paul Dano , John Turturro"}
+};
+
+nlohmann::json film3{
+  {"name", "The Contractor"},
+  {"coutry creater", "USA"},
+  {"create date", 2022},
+  {"director", "Tarik Saleh"},
+  {"actors", "Chris Pine, Gillian Jacobs, Ben Foster, Eddie Marsan, Fares Fares "}
+};
+
+nlohmann::json film4{
+  {"name", "Liked"},
+  {"country creater", "USA"},
+  {"create date", 2019},
+  {"director","Marja-Lewis Ryan"},
+  {"actors", "Virginia Gardner , Logan Paul, Betsy Sodaro ,Sam Lerner, Amy Okuda"}
+};
+
+nlohmann::json film5{
+  {"name", "Fate of a Sport "},
+  {"country creater","USA"},
+  {"create date", 2022},
+  {"director"," Michael Doneger"},
+  {"actors", "Jeffrey Wright, Peter Berg, Seth Meyers , Dylan Dreyer"}
+};
